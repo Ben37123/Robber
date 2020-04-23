9b32d8eabe1423b387671db3561fcc542cbe4662
SecondBen <64219956+SecondBen@users.noreply.github.com>
2020-04-23 10:23:40 -0700

    Update Robber.py

--- Robber.py
+++ Robber.py
@@ -1,1 +1,18 @@
+class Robber(object):
 
+    def __init__(self):
+        self.__isLiving = True
+        self.__isJumping = False
+        self.__direction = 'forward'
+
+    def go_right(self):
+        pass
+
+    def go_left(self):
+        pass
+
+    def jump(self):
+        pass
+
+    def die(self):
+        pass
